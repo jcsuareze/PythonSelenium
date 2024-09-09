809fd44d2c5676b04471d1611e91050f170920bd
jcsuareze <suareze.jcarlos@outlook.com>
2024-09-08 20:49:24 -0600

    añadiendo archivo Holamundo.py

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,2 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# Instanciamos un objeto 'webdriver' de chrome
+driver = webdriver.Chrome()
+
+# Accedemos a la URL de la página
+driver.get("https://demoqa.com/text-box")
+print("Bienvenido a Selenium")
+
+# Obtenemos el elemento de la página
+print(driver.title)
+
+# cerrar el navegador
+driver.close()
+
+
+
+
+
+
 
